f4b0e2a25bec7b08abc30910a48e9dfbe3daaabe
Deniz <mdcengiz@gmail.com>
2020-05-21 14:55:22 -0500

    Set up Core Data

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return itemArray.count
+       }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+      override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+
+    //        context.delete(itemArray[indexPath.row])
+    //        itemArray.remove(at: indexPath.row)
+            
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+            saveItems()
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+        }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+
+    }
+    
+    func saveItems() {
+           
+           do {
+             try context.save()
+           } catch {
+              print("Error saving context \(error)")
+           }
+           
+           self.tableView.reloadData()
+       }
+}
+
